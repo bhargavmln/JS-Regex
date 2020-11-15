6b17560e107b0917b46c2cdf4adf2224c73995f0
bhargavmln <bhargavmln@gmail.com>
2020-11-15 16:41:45 +0530

    [bhargavmln]add:ability to validate even if there is a single space

--- Regex.js
+++ Regex.js
@@ -1,9 +1,13 @@
-let pinRegex = RegExp('^[1-9]{1}[0-9]{5}$')
+//Validate PIN
+let pinRegex = RegExp('^[1-9]{1}[0-9]{2}[\\s]?[0-9]{3}$')
 function validatePin(pin) {
     if(pinRegex.test(pin)) return "Valid pin";
     else return "Invalid pin";
 }
 
-console.log(validatePin(400088));
-console.log(validatePin('A400088'));
-console.log(validatePin('400088B'));
+console.log(validatePin(400088));//UC1
+console.log(validatePin('A400088'));//UC2
+console.log(validatePin('400088B'));//UC3
+console.log(validatePin('400 088'));//UC4
+
+
