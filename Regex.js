a1ec9643c8b7d28c0085fc4037cc43db1cc7496f
bhargavmln <bhargavmln@gmail.com>
2020-11-15 16:59:22 +0530

    [bhargavmln]add:ability to validate mandatory third part

--- Regex.js
+++ Regex.js
@@ -11,9 +11,9 @@ console.log(validatePin('400088B'));//UC3
 console.log(validatePin('400 088'));//UC4
 
 //Validate Email
-let emailRegex  = RegExp('^[a-z0-9]+[@]{1}[A-Za-z0-9]+')
+let emailRegex  = RegExp('^[a-z0-9]+[@]{1}[A-Za-z0-9]+[.]{1}[a-z]{2,}$')
 function validateEmail(email) {
     if(emailRegex.test(email)) return "Valid email";
     else return "Invalid email";
 }
-console.log(validateEmail('abc100@Bridgelabz123'));//UC2
+console.log(validateEmail('abc100@Bridgelabz123.com'));//UC2
